89104e614a1ca107e566fea0d2bb68cd62cd0682
Sam Davenport <samueldavenport@att.net>
2021-10-27 17:22:05 -0500

    user can now search for a city name in form and json data is logged into the console

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,3 +1,21 @@
+var userFormEl = document.querySelector(".form-holder");
+var cityInputEl = document.querySelector(".cityInput");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    // get value from input element
+    var cityname = cityInputEl.value.trim();
+
+    if (cityname) {
+        getCityWeather(cityname);
+        cityInputEl.value = "";
+    } else {
+        alert("Please enter a valid city name");
+    }
+}
+
+
+
 var getCityWeather = function(city) {
     // format the open weather api url
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=9ac94d5206a0a04e92ba7cbf64fe39f8";
@@ -12,4 +30,4 @@ var getCityWeather = function(city) {
     
 }
 
-getCityWeather("Fort Scott");
+userFormEl.addEventListener("submit", formSubmitHandler);
