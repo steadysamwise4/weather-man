0a3cbf5270117f51adaf63d06c7a5b6c3aae9789
Sam Davenport <samueldavenport@att.net>
2021-10-27 16:58:37 -0500

    styling problems fixed

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,6 +1,15 @@
-var getCityWeather = function() {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=9ac94d5206a0a04e92ba7cbf64fe39f8";
-    fetch(apiUrl);
+var getCityWeather = function(city) {
+    // format the open weather api url
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=9ac94d5206a0a04e92ba7cbf64fe39f8";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+    
+    
 }
 
-getCityWeather();
+getCityWeather("Fort Scott");
