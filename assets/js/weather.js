3d0f8f73b18d861393fc23e9ef180c0dbbda2de7
Sam Davenport <samueldavenport@att.net>
2021-10-27 20:01:03 -0500

    now recieves state code info from user, city, state and weather info are console logged when search button is pressed

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,16 +1,28 @@
 var userFormEl = document.querySelector(".form-holder");
 var cityInputEl = document.querySelector(".cityInput");
+var stateInputEl = document.querySelector("#stateInput");
+
+var displayCityData = function(currentWeather, searchTerm) {
+    console.log(currentWeather);
+    console.log(searchTerm);
+}
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
     // get value from input element
-    var cityname = cityInputEl.value.trim();
-
-    if (cityname) {
-        getCityWeather(cityname);
+    var cityName = cityInputEl.value.trim();
+    var stateName = "US-" + stateInputEl.value.trim();
+    var cityState = cityName + "," + stateName
+    if (cityState) {
+        getCityWeather(cityState);
+        cityInputEl.value = "";
+        stateInputEl.value = "";
+    } else if (!stateName) {
+        cityState = cityName;
+        getCityWeather(cityName);
         cityInputEl.value = "";
     } else {
-        alert("Please enter a valid city name");
+    alert("Please enter a valid city name");
     }
 }
 
@@ -18,12 +30,12 @@ var formSubmitHandler = function(event) {
 
 var getCityWeather = function(city) {
     // format the open weather api url
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=9ac94d5206a0a04e92ba7cbf64fe39f8";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city +"&units=imperial&appid=9ac94d5206a0a04e92ba7cbf64fe39f8";
 
     // make a request to the url
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
-            console.log(data);
+            displayCityData(data, city);
         });
     });
     
