ba7f46089a17190d2f4d0bc68d1fc42f518c15a7
Sam Davenport <samueldavenport@att.net>
2021-10-27 23:19:15 -0500

    City name, current date and time, weather condition icon, and current temp now display to the page on button click

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,27 +1,63 @@
 var userFormEl = document.querySelector(".form-holder");
 var cityInputEl = document.querySelector(".cityInput");
 var stateInputEl = document.querySelector("#stateInput");
+var cityHeadingEl = document.querySelector("#citySearch");
+var cityContentEl = document.querySelector(".city-content");
+var cityDataEl = document.querySelector(".city-data");
+var cityIconEl = document.querySelector(".icon");
+var cityTempEl = document.querySelector(".temp");
+var cityHumidityEl = document.querySelector(".humid");
+var cityWindEL = document.querySelector(".wind");
+var cityUltraEL = document.querySelector(".ultra-v");
+var currentTimeEl = document.querySelector("#currentTime");
 
 var displayCityData = function(currentWeather, searchTerm) {
+    // clear old content
+    cityHeadingEl.textContent = searchTerm;
+    cityDataEl.textContent = "";
     console.log(currentWeather);
     console.log(searchTerm);
+    // format time
+    var timeEl = moment().format('MMMM Do YYYY, h:mm a');
+    // append time to dom
+    currentTimeEl.textContent = " - " + timeEl;
+    
+        // format icon
+        var icon = currentWeather.weather[0].icon;
+        var iconEl = document.createElement("img");
+        iconEl.setAttribute("src", "http://openweathermap.org/img/w/"+ icon + ".png");
+        // append
+        
+        cityDataEl.appendChild(iconEl);
+    
+    // format temp
+    var temp = Math.round(currentWeather.main.temp);
+    // create a container for temp
+    var currentTempEl = document.createElement("p");
+    
+    currentTempEl.textContent = "Currently: " + temp + "°F";
+    // append temp to the dom
+    cityDataEl.appendChild(currentTempEl);
+
+    
 }
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
     // get value from input element
     var cityName = cityInputEl.value.trim();
-    var stateName = "US-" + stateInputEl.value.trim();
+    var stateInput = stateInputEl.value.trim();
+    var stateName = "US-" + stateInput;
     var cityState = cityName + "," + stateName
-    if (cityState) {
+    if (cityName && stateInput) {
         getCityWeather(cityState);
         cityInputEl.value = "";
         stateInputEl.value = "";
-    } else if (!stateName) {
+    } else if (cityName && !stateInput) {
         cityState = cityName;
         getCityWeather(cityName);
         cityInputEl.value = "";
-    } else {
+    } else if (!cityName && !stateInput) {
     alert("Please enter a valid city name");
     }
 }
